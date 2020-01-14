ce012090fab0180f3d2436d8ffb309a2226bfb65
Eduardo Cerritos Licea <ecerritos@biometriaaplicada.com>
2020-01-14 00:32:40 -0600

    Version inicial

---
+++ SerializerObject/SerializerObject/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerializerObject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                List<Versionamiento> lst = new List<Versionamiento>
+                {
+                    new Versionamiento {Version = "1.0", Archivo = "1.jpg"},
+                    new Versionamiento {Version = "2.0", Archivo = "2.jpg"},
+                    new Versionamiento {Version = "3.0", Archivo = "3.jpg"},
+                    new Versionamiento {Version = "4.0", Archivo = "4.jpg"},
+                    new Versionamiento {Version = "5.0", Archivo = "5.jpg"},
+                    new Versionamiento {Version = "6.0", Archivo = "6.jpg"},
+                    new Versionamiento {Version = "7.0", Archivo = "7.jpg"}
+                };
+                new NegocioSerializer.ArchivoXml<Versionamiento>().SerializarLista(lst, @"C:\Prueba.xml");
+
+                List<Versionamiento> lstArchivo =
+                    new NegocioSerializer.ArchivoXml<Versionamiento>().CargarListaArchivo(@"C:\Prueba.xml");
+
+                Versionamiento v =
+                    new NegocioSerializer.ArchivoXml<Versionamiento>().CargarObjectoArchivo(@"C:\Prueba.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public class Versionamiento
+        {
+            public string Version { get; set; }
+            public string Archivo { get; set; }
+            public string Beta { get; set; }
+        }
+    }
+}
---
+++ SerializerObject/SerializerObject/NegocioSerializer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SerializerObject
+{
+    public class NegocioSerializer
+    {
+        public class CadenaXml<T>
+        {
+            readonly StringBuilder _sbData;
+            StringWriter _swWriter;
+            XmlDocument _xDoc;
+            XmlNodeReader _xNodeReader;
+            XmlSerializer _xmlSerializer;
+            public CadenaXml()
+            {
+                _sbData = new StringBuilder();
+            }
+
+            public string SerializarDatos(T datos)
+            {
+                try
+                {
+                    XmlSerializer employeeSerializer = new XmlSerializer(typeof(T));
+                    _swWriter = new StringWriter(_sbData);
+                    employeeSerializer.Serialize(_swWriter, datos);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                return _sbData.ToString();
+            }
+
+            public T DeserializarDatos(string cadenaXml)
+            {
+                T resultado = default(T);
+                try
+                {
+                    _xDoc = new XmlDocument();
+                    _xDoc.LoadXml(cadenaXml);
+
+                    if (_xDoc.DocumentElement != null)
+                    {
+                        _xNodeReader = new XmlNodeReader(_xDoc.DocumentElement);
+                        _xmlSerializer = new XmlSerializer(typeof(T));
+                        var employeeData = _xmlSerializer.Deserialize(_xNodeReader);
+                        resultado = (T)employeeData;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                return resultado;
+            }
+        }
+
+        public class ArchivoXml<T>
+        {
+            public void SerializarObjeto(T objeto, string archivo)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                using (TextWriter writer = new StreamWriter(archivo))
+                {
+                    serializer.Serialize(writer, objeto);
+                }
+            }
+            public void SerializarLista(List<T> lista, string archivo)
+            {
+                try
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                    using (TextWriter writer = new StreamWriter(archivo))
+                    {
+                        serializer.Serialize(writer, lista);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            public T CargarObjectoArchivo(string archivo)
+            {
+                T result;
+                try
+                {
+                    XmlSerializer reader = new XmlSerializer(typeof(T));
+                    StreamReader file = new StreamReader(archivo);
+                    result = (T)reader.Deserialize(file);
+                    file.Close();
+                }
+                catch (Exception ex)
+                {
+                    result = default(T);
+                    throw new Exception(ex.Message);
+                }
+                return result;
+            }
+
+            public List<T> CargarListaArchivo(string archivo)
+            {
+                List<T> result;
+                try
+                {
+                    XmlSerializer reader = new XmlSerializer(typeof(List<T>));
+                    StreamReader file = new StreamReader(archivo);
+                    result = (List<T>)reader.Deserialize(file);
+                    file.Close();
+                }
+                catch (Exception ex)
+                {
+                    result = default(List<T>);
+                    throw new Exception(ex.Message);
+                }
+                return result;
+            }
+        }
+    }
+}
